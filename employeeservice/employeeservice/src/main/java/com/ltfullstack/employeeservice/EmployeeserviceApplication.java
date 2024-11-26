0e96bcd6ec940d29c2247dede7c7a262eaa4c67b
Van Chi Hieu <vanchihieu3@gmail.com>
2024-11-26 22:40:26 +0700

    Khởi Tạo Và Xây Dựng Cấu Trúc Cho EmployeeService

--- employeeservice/employeeservice/src/main/java/com/ltfullstack/employeeservice/EmployeeserviceApplication.java
+++ employeeservice/employeeservice/src/main/java/com/ltfullstack/employeeservice/EmployeeserviceApplication.java
@@ -1,17 +1,15 @@
-package com.ltfullstack.bookservice;
+package com.ltfullstack.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@ComponentScan({"com.ltfullstack.bookservice","com.commonservice"})
-public class BookserviceApplication {
+public class EmployeeserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookserviceApplication.class, args);
+		SpringApplication.run(EmployeeserviceApplication.class, args);
 	}
 
 }
