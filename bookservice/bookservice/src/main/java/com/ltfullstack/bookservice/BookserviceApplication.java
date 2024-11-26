57404e612dc9feaf49e11ec842bb86cf53454f56
Van Chi Hieu <vanchihieu3@gmail.com>
2024-11-26 21:30:43 +0700

    Xử Lý Ngoại Lệ - Exception Handling

--- bookservice/bookservice/src/main/java/com/ltfullstack/bookservice/BookserviceApplication.java
+++ bookservice/bookservice/src/main/java/com/ltfullstack/bookservice/BookserviceApplication.java
@@ -3,9 +3,11 @@ package com.ltfullstack.bookservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+//@ComponentScan({"com.ltfullstack.bookservice","com.commonservice"})
 public class BookserviceApplication {
 
 	public static void main(String[] args) {
