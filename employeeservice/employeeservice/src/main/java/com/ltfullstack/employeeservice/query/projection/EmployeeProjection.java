b885d673d9b8d09b5eec0e68db5b85b44db4fe64
Van Chi Hieu <vanchihieu3@gmail.com>
2024-11-27 10:00:57 +0700

    Xây Dựng API Filter Nhân Viên - Áp Dụng Spring Data JPA Query

---
+++ employeeservice/employeeservice/src/main/java/com/ltfullstack/employeeservice/query/projection/EmployeeProjection.java
@@ -0,0 +1,39 @@
+package com.ltfullstack.employeeservice.query.projection;
+
+import com.commonservice.model.EmployeeResponseCommonModel;
+import com.commonservice.queries.GetDetailEmployeeQuery;
+import com.ltfullstack.employeeservice.command.data.Employee;
+import com.ltfullstack.employeeservice.command.data.EmployeeRepository;
+import com.ltfullstack.employeeservice.query.model.EmployeeResponseModel;
+import com.ltfullstack.employeeservice.query.queries.GetAllEmployeeQuery;
+import org.axonframework.queryhandling.QueryHandler;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class EmployeeProjection {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @QueryHandler
+    public List<EmployeeResponseModel> handle(GetAllEmployeeQuery query){
+        List<Employee> listEmployee = employeeRepository.findAllByIsDisciplined(query.getIsDisciplined());
+        return listEmployee.stream().map(employee -> {
+            EmployeeResponseModel model = new EmployeeResponseModel();
+            BeanUtils.copyProperties(employee,model);
+            return model;
+        }).toList();
+    }
+
+    @QueryHandler
+    public EmployeeResponseCommonModel handle(GetDetailEmployeeQuery query) throws Exception{
+        Employee employee = employeeRepository.findById(query.getId()).orElseThrow(() -> new Exception("Employee not found"));
+        EmployeeResponseCommonModel model = new EmployeeResponseCommonModel();
+        BeanUtils.copyProperties(employee,model);
+        return model;
+    }
+}
