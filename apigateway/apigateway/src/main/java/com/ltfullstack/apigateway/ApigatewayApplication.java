109e5dcc738a78aeb714dd00821897de2064776f
Van Chi Hieu <vanchihieu3@gmail.com>
2024-11-27 23:00:17 +0700

    Viết Custom Gateway Filter Để Authentication Và Authorization

--- apigateway/apigateway/src/main/java/com/ltfullstack/apigateway/ApigatewayApplication.java
+++ apigateway/apigateway/src/main/java/com/ltfullstack/apigateway/ApigatewayApplication.java
@@ -3,6 +3,9 @@ package com.ltfullstack.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +13,11 @@ public class ApigatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+
+	@Bean
+	public KeyResolver keyResolver(){
+		return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
 	}
 
 }
